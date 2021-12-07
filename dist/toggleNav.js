9869d0a6fa5d732e25888af0ed2f46ca88039faa
p-alex <alexey9021@gmail.com>
2021-12-07 19:05:05 +0200

    Add: backdrop to mobile navigation menu

--- dist/toggleNav.js
+++ dist/toggleNav.js
@@ -1,9 +1,14 @@
 var hamburger = document.querySelector(".nav__hamburger");
 var navContainer = document.querySelector(".nav__links");
 var closeBtn = document.querySelector(".nav__close");
+var mobileBackdrop = document.querySelector(".nav__links__mobile__backdrop");
 hamburger.addEventListener("click", function () {
     navContainer.classList.add("nav-active");
+    mobileBackdrop.classList.add("nav-active");
 });
-closeBtn.addEventListener("click", function () {
+function closeMobileNav() {
     navContainer.classList.remove("nav-active");
-});
+    mobileBackdrop.classList.remove("nav-active");
+}
+closeBtn.addEventListener("click", function () { return closeMobileNav(); });
+mobileBackdrop.addEventListener("click", function () { return closeMobileNav(); });
--- src/toggleNav.ts
+++ src/toggleNav.ts
@@ -3,11 +3,20 @@ const hamburger = document.querySelector(
 ) as HTMLButtonElement;
 const navContainer = document.querySelector(".nav__links") as HTMLUListElement;
 const closeBtn = document.querySelector(".nav__close") as HTMLButtonElement;
+const mobileBackdrop = document.querySelector(
+  ".nav__links__mobile__backdrop"
+) as HTMLDivElement;
 
 hamburger.addEventListener("click", (): void => {
   navContainer.classList.add("nav-active");
+  mobileBackdrop.classList.add("nav-active");
 });
 
-closeBtn.addEventListener("click", (): void => {
+function closeMobileNav(): void {
   navContainer.classList.remove("nav-active");
-});
+  mobileBackdrop.classList.remove("nav-active");
+}
+
+closeBtn.addEventListener("click", (): void => closeMobileNav());
+
+mobileBackdrop.addEventListener("click", (): void => closeMobileNav());
